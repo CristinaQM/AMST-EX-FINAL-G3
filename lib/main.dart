9b78d63ee7e3cabc0399ddc54f7b92a47a6faa2a
nisenare <nisenare@espol.edu.ec>
2023-09-01 20:24:36 -0500

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:get/get.dart';
 
 import 'core/theme/app_themes.dart';
 import 'routes/pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
